2adba829742f0237fad8d128bcbedebe16606db5
Moulyasree1234 <143620769+Moulyasree1234@users.noreply.github.com>
2024-09-17 19:41:30 +0000

    Completed Movie Script Analysis Assignment

--- src/main/java/com/movie/script/analysis/DialogueLengthReducer.java
+++ src/main/java/com/movie/script/analysis/DialogueLengthReducer.java
@@ -8,8 +8,15 @@ import java.io.IOException;
 
 public class DialogueLengthReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
 
+  
     @Override
     public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        int totalLength = 0;
+        for (IntWritable val : values) {
+            totalLength += val.get();
+        }
+        context.write(key, new IntWritable(totalLength));
 
     }
 }
+
